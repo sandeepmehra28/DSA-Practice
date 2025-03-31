7b3a24055ce417e14e1d21104cf16bad0eb9e58b
sandeep mehra <sandeepmehra2259@gmail.com>
2025-03-31 23:29:38 +0530

    Update practice.java

--- practice/BinarySearch/BinarySearchQuestions/practice.java
+++ practice/BinarySearch/BinarySearchQuestions/practice.java
@@ -4,11 +4,29 @@ public class practice {
     public static void main(String[] args) {
         System.out.println("this is my practice file");
     }
-    static  int find(int[] arr){
+    static  int find(int[] arr,int target){
         int start = 0;
         int end = arr.length-1;
+        boolean isAs = arr[start]<=arr[end];
         while(start<=end){
             int mid  = start+(end-start)/2;
+            if(target==arr[mid]){
+                return mid;
+        }   
+            //if condition is true
+            if(isAs){
+                if(target<arr[mid]){
+                    end =mid-1;
+                 }else{
+                    start = mid+1;
+                }
+                }else{
+                if(target>arr[mid]){
+                   start = mid+1;
+                }else{
+                    end= mid-1; 
+                }
+            }
         }
         return -1;
     }
