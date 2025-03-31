7de929efc98d00093dc32099556036c986780291
sandeep mehra <sandeepmehra2259@gmail.com>
2025-03-31 12:44:20 +0530

    new file

---
+++ practice/BinarySearch/BinarySearchQuestions/practice.java
@@ -0,0 +1,15 @@
+package BinarySearch.BinarySearchQuestions;
+
+public class practice {
+    public static void main(String[] args) {
+        System.out.println("this is my practice file");
+    }
+    static  int find(int[] arr){
+        int start = 0;
+        int end = arr.length-1;
+        while(start<=end){
+            int mid  = start+(end-start)/2;
+        }
+        return -1;
+    }
+}
